b141b82f809b6763e00eb55486b62a9768198bc1
Ben Brooks <ben@bbrks.me>
2013-02-01 02:11:44 +0000

    commented out broken emailing

--- N06/src/Main/src/java/monsterMashGroupProject/RegisterInfo.java
+++ N06/src/Main/src/java/monsterMashGroupProject/RegisterInfo.java
@@ -68,9 +68,10 @@ public class RegisterInfo {
         
         log.Register(regUsername, regPassword, regEmail);
         
-        UserEmail email = new UserEmail();
-        email.send("felix.farquharson@googlemail.com", "monster-mash-n06@teame4t.co.uk",
-                "User Registration", "Sucessfully registered");
+        // This breaks stuff!!
+        //UserEmail email = new UserEmail();
+        //email.send("felix.farquharson@googlemail.com", "monster-mash-n06@teame4t.co.uk",
+        //        "User Registration", "Sucessfully registered");
         
         return "login.jsp";
     }
